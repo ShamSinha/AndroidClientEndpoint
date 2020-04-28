5b2dfdf13e0f0891ef4d34a9b01e776a9fd642ca
Shubham Kumar <shamsinha1909@gmail.com>
2020-04-28 08:47:33 +0530

    Initial commit

---
+++ app/src/main/java/com/example/newclient/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.newclient;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
+
+import javax.websocket.Session;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText ;
+    String message = "Hello";
+    TextView textView3 ;
+    TextView textView4 ;
+    TextView textView6;
+    MyClientEndpoint myClientEndpoint  = new MyClientEndpoint();
+    Session session ;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main2);
+        editText = (EditText) findViewById(R.id.editText2);
+        textView3 = (TextView) findViewById(R.id.textView3);
+
+        textView4 = (TextView) findViewById(R.id.textView4);
+        textView4.setText("Not Connected To Server");
+        textView6 = (TextView) findViewById(R.id.textView6) ;
+        textView6.setText("");
+
+    }
+
+
+    public void send(View view) {
+
+        message  = editText.getText().toString() ;
+
+        myClientEndpoint.SendMessageToServer(message,textView3);
+
+
+     /*   Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    myClientEndpoint.SendMessage(message);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();*/
+
+    }
+
+
+
+    public void onClickbutton(View view)  {
+
+        session = myClientEndpoint.ConnectClientToServer(textView4);
+        //textView4.setText(MyClientEndpoint.session.getId());
+
+    }
+
+
+
+}
+
+
+
+
+
---
+++ app/src/main/java/com/example/newclient/MyClientEndpoint.java
@@ -0,0 +1,101 @@
+package com.example.newclient;
+
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.websocket.ClientEndpoint;
+import javax.websocket.ClientEndpointConfig;
+import javax.websocket.ContainerProvider;
+import javax.websocket.DeploymentException;
+import javax.websocket.Endpoint;
+import javax.websocket.EndpointConfig;
+import javax.websocket.MessageHandler;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.WebSocketContainer;
+
+
+
+@ClientEndpoint
+class MyClientEndpoint {
+
+    private Session session1;
+    private String EchoFromServer ;
+
+    Session ConnectClientToServer(final TextView text) {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+              connectToWebSocket(text);
+            }
+            });
+        thread.start();
+        return session1;
+    }
+
+    @OnOpen
+    public void onOpen(Session session){
+        session1 = session ;
+    }
+
+    @OnMessage
+    public void onMessage(String incomingMessage){
+        EchoFromServer = incomingMessage ;
+    }
+
+
+    private void connectToWebSocket(TextView text) {
+        WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+
+            URI uri = URI.create("ws://echo.websocket.org");
+        try {
+            session1 = container.connectToServer(this, uri);
+            if(session1 != null){
+                text.setText("Connected to Session : \n" + session1.getId());
+            }
+        } catch (DeploymentException e) {
+            e.printStackTrace();
+            text.setText("Deployment Exception");
+        } catch (IOException e) {
+            e.printStackTrace();
+            text.setText("IO Exception");
+        }
+
+    }
+
+    void SendMessageToServer(final String message, final TextView textView1) {
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    session1.getBasicRemote().sendText(message);
+                } catch (IOException e) {
+                    textView1.setText("IOException in sending Message");
+                    e.printStackTrace();
+                }
+                textView1.setText(EchoFromServer);
+            }
+        });
+        thread1.start();
+    }
+}
+//"ws://echo.websocket.org"
+
+    /*MainActivity m = new MainActivity();
+
+    @OnOpen
+    public void onOpen(Session session){
+       this.session = session;
+    }
+
+    @OnMessage
+    public void onMessage(String incomingMessage){
+        m.textView3.setText(String.format("%s   %s", session.getId(), incomingMessage));
+    }*/
+
+
+
